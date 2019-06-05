59bfbc09d684d9bbf18be360b24204d60dc8a7db
krava <boga11a@yandex.ru>
2019-06-05 20:45:03 +0300

    homework4 is done

---
+++ app/src/main/java/com/krava11/moneytracker/ItemsAdapter.java
@@ -0,0 +1,85 @@
+package com.krava11.moneytracker;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ItemViewHolder> {
+
+    private List<Item> data = new ArrayList<>();
+
+    public ItemsAdapter() {
+        createData();
+    }
+
+    @NonNull
+    @Override
+    public ItemsAdapter.ItemViewHolder onCreateViewHolder(ViewGroup parent, int i) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_record1, parent, false);
+
+//        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+//        View view1 = inflater.inflate(R.layout.item_record1, parent, false);
+
+        return new ItemsAdapter.ItemViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ItemsAdapter.ItemViewHolder viewHolder, int i) {
+        Item item = data.get(i);
+        viewHolder.applyData(item);
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    private void createData() {
+        data.add(new Item("Молоко", 100));
+        data.add(new Item("Жизнь", 200));
+        data.add(new Item("Хлеб", 30));
+        data.add(new Item("Курсы", 30000));
+        data.add(new Item("Тот самый ужин который я оплатил за всех картой", 4600));
+        data.add(new Item("", 0));
+        data.add(new Item("Ужин с любимой женой", 987));
+        data.add(new Item("ракета", 90));
+        data.add(new Item("Тысячелетний сокол", 100000000));
+        data.add(new Item("Macbook Pro", 240000));
+        data.add(new Item("Машина", 620000));
+        data.add(new Item("Молоко", 100));
+        data.add(new Item("Жизнь", 200));
+        data.add(new Item("Хлеб", 30));
+        data.add(new Item("Курсы", 30000));
+        data.add(new Item("Тот самый ужин который я оплатил за всех картой", 4600));
+        data.add(new Item("", 0));
+        data.add(new Item("Ужин с любимой женой", 987));
+        data.add(new Item("ракета", 90));
+        data.add(new Item("Тысячелетний сокол", 100000000));
+        data.add(new Item("Macbook Pro", 240000));
+        data.add(new Item("Машина", 620000));
+    }
+
+    static class ItemViewHolder extends RecyclerView.ViewHolder {
+        private final TextView title;
+        private final TextView price;
+
+        public ItemViewHolder(@NonNull View itemView) {
+            super(itemView);
+            title = itemView.findViewById(R.id.title);
+            price = itemView.findViewById(R.id.price);
+        }
+
+        public void applyData(Item item) {
+            title.setText(item.getTitle());
+            price.setText(String.format("%s\u20BD",String.valueOf(item.getPrice())));
+        }
+    }
+
+}
--- app/src/main/java/com/krava11/moneytracker/MainActivity.java
+++ app/src/main/java/com/krava11/moneytracker/MainActivity.java
@@ -1,15 +1,31 @@
 package com.krava11.moneytracker;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ViewPager pager;
+    private TabLayout tabLayout;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pager = findViewById(R.id.viewPager);
+        tabLayout = findViewById(R.id.tabLayout);
+
+        MainPagesAdapter adapter = new MainPagesAdapter(getSupportFragmentManager(),this);
+        pager.setAdapter(adapter);
+
+        tabLayout.setupWithViewPager(pager);
+
+
     }
 
     @Override
